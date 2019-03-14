7ea97c99e4fd93222b85171ad1f0d27a24096f49
kongdezhao2357 <44801810+kongdezhao2357@users.noreply.github.com>
2019-03-14 14:29:07 +0800

    Update pipelines.py

--- liepinSpd2/liepinSpd2/pipelines.py
+++ liepinSpd2/liepinSpd2/pipelines.py
@@ -60,8 +60,8 @@ class Liepinspd2Pipeline(object):
 
     def __init__(self):
         # 建立连接
-        self.conn = pymysql.connect('rm-2zewagytttzk6f24xno.mysql.rds.aliyuncs.com', 'cn_ainvest_db', 'cn_ainvest_sd3a1', 'special_data')  # 有中文要存入数据库的话要加charset='utf8'
-        # 创建游标
+        self.conn = pymysql.connect(...)
+            # 创建游标
         self.cursor = self.conn.cursor()
 
     def process_item(self, item, spider):
